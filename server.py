c362238a39dc043206fd63589a95b11aa93a0dba
Startooth <444248631@qq.com>
2020-08-24 15:04:24 +0800

    项目初始化

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask import render_template
+
+from libs.orm import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:zysqwxfx0714@localhost:3306/Blog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.secret_key = r'dsaf^%*EfdfggfghdnydbrdsdasdadjhGH9-ua'
+
+
+db.init_app(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    manager.run()
